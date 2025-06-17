a78d4b0cd274258b4802722f5dccd99701bee272
selicode1 <amegashieselikem@gmail.com>
2025-06-17 19:28:13 +0000

    env

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,16 +9,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCqip6Jq2lugr1KvkBQMvA1enCffUNwH-E",
-  authDomain: "mhealth-6191e.firebaseapp.com",
-  databaseURL: "https://mhealth-6191e-default-rtdb.firebaseio.com",
-  projectId: "mhealth-6191e",
-  storageBucket: "mhealth-6191e.appspot.com",
-  messagingSenderId: "346868082875",
-  appId: "1:346868082875:web:502fac14c68d4ba8c7e19e",
-  measurementId: "G-75BPJD6QVY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
